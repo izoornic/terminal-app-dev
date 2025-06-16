994be51738b6a660835d09199148c547462d6883
izoornic <izornic@gmail.com>
2025-06-16 16:03:37 +0200

    V 1.0.3.7.1

--- app/Http/Controllers/Api/LicenceController.php
+++ app/Http/Controllers/Api/LicenceController.php
@@ -116,7 +116,7 @@ class LicenceController extends Controller
     }
 
     private function getGatumKrajLicence($terminal_lokacijaId, $distributerId, $licenca_distributer_cenaId){
-        return LicencaNaplata::select('datum_kraj_licence')
+        $datum_kraj = LicencaNaplata::select('datum_kraj_licence')
                 ->where([
                     'terminal_lokacijaId' => $terminal_lokacijaId, 
                     'distributerId' => $distributerId, 
@@ -124,5 +124,6 @@ class LicenceController extends Controller
                     'aktivna' => 1
                 ])
                 ->first()->datum_kraj_licence;
+         return  ($datum_kraj) ? $datum_kraj->format('Y-m-d') : '';  
     }
 }
--- config/global.php
+++ config/global.php
@@ -4,7 +4,7 @@ return [
 
 	'siteFooter' => '© '.date("Y").' Copyright: Zeta System EPOS',
 
-	'version' => 'V 1.0.3.7',
+	'version' => 'V 1.0.3.7.1',
 
 	'paginate' => 10,
 
