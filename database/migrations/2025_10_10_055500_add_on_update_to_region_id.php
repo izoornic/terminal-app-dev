354f89138de109947eba6dc17f1045d6f2e4bf22
izoornic <izornic@gmail.com>
2025-10-16 18:39:24 +0200

    V 1.0.6.6

--- database/migrations/2025_10_10_055500_add_on_update_to_region_id.php
+++ database/migrations/2025_10_10_055500_add_on_update_to_region_id.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::table('blokacijas', function (Blueprint $table) {
             //
             //$table->foreignId('bankomat_region_id')->change()->nullable()->cascadeOnUpdate();
+            $table->dropForeign('blokacijas_bankomat_region_id_foreign');
+            $table->dropForeign('blokacija_tip_id');
             $table->foreignId('bankomat_region_id')->change()->nullable()->constrained('bankomat_regions')->onUpdate('cascade')->onDelete('set null');
             $table->foreignId('blokacija_tip_id')->change()->nullable()->constrained('blokacija_tips')->onUpdate('cascade')->onDelete('set null');
         });
